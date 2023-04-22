ec5e43ad1dcf55694a6fde2371845fdb53d55d49
Valerii219 <valera.arl96@gmail.com>
2023-04-22 17:53:31 +0200

    1 task

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,11 +1,8 @@
-
 const categoryEl = document.querySelectorAll('.item');
 
-console.log(`Number of categories: ${categoryEl.length}
-`);
-categoryEl.forEach
+console.log(`Number of categories: ${categoryEl.length}`);
 
-
-
-
-
+categoryEl.forEach(categ => {
+  console.log(`Category: ${categ.firstElementChild.textContent}`);
+  console.log(`Elements: ${categ.lastElementChild.querySelectorAll(`li`).length}`);
+});
