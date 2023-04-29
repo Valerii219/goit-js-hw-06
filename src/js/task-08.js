5f7f89f018cd8ee82a81b7682278c0529db27cda
Valerii219 <valera.arl96@gmail.com>
2023-04-29 15:46:43 +0200

    Number 8+

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,22 @@
+const form = document.querySelector('.login-form');
+
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit (event) {
+event.preventDefault();
+const formCurrent = event.currentTarget.elements;
+const email = formCurrent.email.value;
+const password = formCurrent.password.value;
+
+if(email === "" || password === ""){
+    alert('всі поля повинні бути заповнені');}
+
+const formData = new FormData(event.currentTarget)
+formData.forEach((email, password) => {
+console.log('name:', password),
+console.log('value:', email)
+})
+
+form.reset(formData)
+}
 
